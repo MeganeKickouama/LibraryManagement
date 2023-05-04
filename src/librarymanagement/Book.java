6d2099156c37e4e4e769d76b7c3406e824024734
Sarah <Sarah@Sarah-PC>
2023-05-04 15:15:30 -0400

    Did borrowBook()

--- src/librarymanagement/Book.java
+++ src/librarymanagement/Book.java
@@ -18,13 +18,13 @@ public class Book {
     private int bookID;
     private int price;
     private static int count = 0;
+    protected boolean isAvailable = true;
     
     //need fields to  borrow a book & return a book
     //like
     //private boolean isBookAvailable or isBookReserved
-            
-
-    public Book( int id, String title, String author, int price)
+        
+    public Book(int id, String title, String author, int price)
     {
         this.bookID = id;
         this.title = title;
